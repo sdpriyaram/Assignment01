c9826200a0e76e21e5a59c4b734bf2e02f27dd28
Priya Ramesh <sdpriyaramesh@gmail.com>
2024-09-20 00:18:40 -0700

    all steps done

---
+++ js/script.js
@@ -0,0 +1,158 @@
+//STEP 1
+/*
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+*/
+//STEP 2
+/*
+let numericLiteral = 4;
+let stringLiteral = 'Test';
+let booleanLiteral = true;
+let nullLiteral = null;
+document.writeln(`${numericLiteral} is the numericLiteral<br>`);
+document.writeln(`${stringLiteral} is the stringLiteral<br>`);
+document.writeln(`${booleanLiteral} is the booleanLiteral<br>`);
+document.writeln(`${nullLiteral} is the nullLiteral<br>`);
+*/
+//STEP 3
+/*
+let complexExpression1 = (3+5)*(9+4)
+let complexExpression2 = (9>5) && (4>6)
+document.writeln(`complex expression 1 - ${complexExpression1}<br>`);
+document.writeln(`complex expression 2 - ${complexExpression2}<br>`);
+*/
+//STEP 4
+/*
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState
+let strZipCode
+let intYourAge
+let strReferralSource
+let boolMayWeContactYou
+*/
+//STEP 5
+/*
+let strFirstName
+let strLastName
+let strAddress
+strFirstName = 'John'
+strLastName = 'Doe'
+strAddress = '12345 Trial st'
+document.writeln(`First name is ${strFirstName}<br>`);
+document.writeln(`Last name is ${strLastName}<br>`);
+document.writeln(`Address is ${strAddress}<br>`);
+let strCity = 'TestCity',
+    strState = 'TestState',
+    strZipCode = '12345';
+document.writeln(`City is ${strCity}<br>`);
+document.writeln(`State is ${strState}<br>`);
+document.writeln(`Zip Code is ${strZipCode}<br>`);
+let intYourAge = 10;
+let strReferralSource = 'Friend';
+let boolMayWeContactYou = false;
+document.writeln(`Age is ${intYourAge}<br>`);
+document.writeln(`Referral Source is ${strReferralSource}<br>`);
+document.writeln(`May we contact you: ${boolMayWeContactYou}<br>`);
+*/
+//STEP 6
+/*
+let varOne = 1 + 'variable';
+console.info(`1 + variable = ${varOne}`);
+*/
+//STEP 7
+/*
+let varOne = true + 'Result';
+let varTwo = 1 + false;
+document.writeln(`varOne = true + Result is ${varOne}<br>`);
+document.writeln(`varTwo = 1 + false is ${varTwo}<br>`);
+*/
+//STEP 8 - by adding an escape character(\) to I'm
+/*
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString)
+*/
+//STEP 9
+/*
+let nullVariable = null;
+console.info(nullVariable);
+let undefinedVariable;
+console.info(undefinedVariable);
+*/
+//STEP 10
+/*
+document.writeln(`typeof 'Testing' is ${typeof('Testing')}<br>`);
+document.writeln(`typeof 5 is ${typeof(5)}<br>`);
+document.writeln(`typeof true is ${typeof(true)}<br>`);
+document.writeln(`typeof new String object is ${typeof(new String('Test'))}<br>`);
+document.writeln(`typeof undefVariable is ${typeof(undefVariable)}<br>`);
+*/
+//STEP 11
+/*
+alert('Hello ' + 'Priya Ramesh' + ' ,welcome to the JavaScript class!');
+*/
+//STEP 12
+/*
+let name = 'Priya Ramesh';
+alert('Hello ' + name + ' ,welcome to the JavaScript class!');
+*/
+//STEP 13
+/*
+let name = 'Priya Ramesh';
+let course = 'Javascript';
+alert('Hello ' + name + ' ,welcome to the ' + course + ' class!');
+*/
+//STEP 14
+/*
+let name = 'Priya Ramesh';
+let course = 'Javascript';
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+*/
+//STEP 15
+/*
+let name = prompt('Enter your name:');
+let course = 'Javascript';
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+*/
+//STEP 16
+/*
+let name = prompt('Enter your name:');
+let course = prompt('Enter the course you are taking:');
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+*/
+//STEP 17
+/*
+let x = 10;
+let y = 20;
+console.info(`Sum of x and y is ${x+y}`);
+*/
+//STEP 18
+/*
+let x = 20;
+console.info(`Value of x += 20 is ${x += 20}`);
+*/
+//STEP 19
+/*
+let x = 20;
+console.info(`Value of x *= 5 is ${x *= 5}`);
+*/
+//STEP 20
+/*
+let x = 20%3;
+console.info(`Value of x /= 1 is ${x /= 1}`);
+*/
+//STEP 21
+/*
+result = (8 > 5) || (4 > 9) || (2 > 6);
+console.info(`Result of (8 > 5) || (4 > 9) || (2 > 6) is ${result}`);
+*/
+//STEP 22
+/*
+result = (5 >= 5) && (4 < 9) && (2 == 6);
+console.info(`Result of (5 >= 5) && (4 != 9) && (2 == 6) is ${result}`);
+*/
